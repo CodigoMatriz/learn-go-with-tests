d221c07c460f934e6a1f9377294f5ff5a35ed4b4
CodigoMatriz <codigomatriz@astrikmedia.com>
2020-06-11 23:45:54 -0700

    Finish Hello, World

--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -12,15 +12,29 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Arturo")
+		got := Hello("Arturo", "")
 		want := "Hello, Arturo"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Pumba", "Spanish")
+		want := "Hola, Pumba"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Felix", "French")
+		want := "Bonjour, Felix"
 
 		assertCorrectMessage(t, got, want)
 	})
