835fd8ed3ac22633556af250c0cdf7722a5fe4d9
CodigoMatriz <codigomatriz@astrikmedia.com>
2020-06-13 00:19:23 -0700

    Add iterations lesson

---
+++ 03-iteration/iteration.go
@@ -0,0 +1,11 @@
+package iteration
+
+func Repeat(character string, iterations int) string {
+	var repeated string
+
+	for i := 0; i < iterations; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
